4aa066a55eee782877dfbd9a47a8e989dd31e513
Adekunle Joshua Adebisi <106779741+Adebisijoshua@users.noreply.github.com>
2023-09-20 13:39:36 -0400

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Usage: ./calculator.sh a b"
+
 printf "Enter the first number: "
 read number1
-printf "Enter the operation (+, -, *, /): "
+printf "Enter the operation (+, -, *, /, %, ^, sin, cos, tan): "
 read operation
 printf "Enter the second number: "
 read number2
@@ -14,6 +14,27 @@ case $operation in
     -)
         result=$((number1 - number2))
         ;;
+    \*)
+        result=$((number1 * number2))
+        ;;
+    /)
+        result=$(echo "scale=2; $number1 / $number2" | bc)
+        ;;
+    %)
+        result=$((number1 % number2))
+        ;;
+    ^)
+        result=$(echo "$number1 ^ $number2" | bc)
+        ;;
+    sin)
+        result=$(echo "s($number1)" | bc -l)
+        ;;
+    cos)
+        result=$(echo "c($number1)" | bc -l)
+        ;;
+    tan)
+        result=$(echo "s($number1) / c($number1)" | bc -l)
+        ;;
     *)
         echo "Invalid operation: $operation"
         exit 1
@@ -22,4 +43,3 @@ esac
 
 echo "$number1 $operation $number2 = $result"
 exit 0
-
